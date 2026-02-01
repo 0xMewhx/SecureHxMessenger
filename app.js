e15b62ac94e10e010b4c2207bbef1d8ff5beed1b
0xMEW <pokox3proyoyo@gmail.com>
2026-02-02 02:55:15 +0500

    Modify ICE servers to add TURN server support
    Updated ICE server configuration to include TURN servers.

--- app.js
+++ app.js
@@ -1396,10 +1396,19 @@ function setupCallListeners() {
 function setupCallPeerConnection() {
   // 1. Инициализируем соединение
   const pc = new RTCPeerConnection({ // Используй RTCPeerConnection (стандарт)
-    iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' },
-      { urls: 'stun:stun.stunprotocol.org:3478' }
-    ]
+iceServers: [
+  { urls: 'stun:stun.l.google.com:19302' },
+  {
+    urls: 'turn:openrelay.metered.ca:80',
+    username: 'openrelayproject',
+    credential: 'openrelayproject'
+  },
+  {
+    urls: 'turn:openrelay.metered.ca:443',
+    username: 'openrelayproject',
+    credential: 'openrelayproject'
+  }
+]
   });
   
   state.callState.pc = pc;
